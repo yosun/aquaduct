1c63e39a4e5ac312e20e30b55b33545290bdf3c9
KirarinSnow <kirarinsnow@KirarinSnows-MacBook-Air.local>
2014-12-07 14:30:35 -0800

    garden negate x

--- garden2/garden.js
+++ garden2/garden.js
@@ -319,6 +319,7 @@ function disconnect(connector) {
 
 function updateData(object) {
 	var url = "http://localhost/bss/aquaduct/server.dump.php";
+	object.data.x = -object.data.x;
 	var data = object.data;
 	//data['x'] = object.position.x;
 //	data['y'] = object.position.y;
